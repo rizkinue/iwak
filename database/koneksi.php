76b2ffe08dd482fb71cf41d7078fedc4fa4c0d93
Ibnu Maulana <89495996+rizkinue@users.noreply.github.com>
2022-12-05 09:14:51 +0700

    main

---
+++ database/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Memanggil database
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db   = "iwak";
+
+    $conn = mysqli_connect($host, $user, $pass, $db);
+
+?>
