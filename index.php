9edd01736981bc3733ff3952fa6f6f69c10e991a
Ibnu Maulana <89495996+rizkinue@users.noreply.github.com>
2022-12-05 09:19:34 +0700

    main

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+
+    // Sebelum masuk index.php, login dulu
+    require_once 'login.php';
+
+    // Jika sudah login, masuk ke index.php
+    require_once 'index.php';
+
+    
+
+?>
--- login.php
+++ login.php
@@ -4,5 +4,63 @@
     session_start();
 
     // Memanggil file koneksi.php  
+    include "koneksi.php";
 
+    // Jika tombol login di klik
+    if(isset($_POST['login'])){
+
+        // Menangkap data dari form login
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // Membuat query untuk mengecek apakah username dan password ada di database
+        $query = mysqli_query($koneksi
+        , "SELECT * FROM user WHERE username='$username' AND password='$password'");
+        $cek = mysqli_num_rows($query);
+
+        // Jika username dan password ada di database
+        if($cek > 0){
+
+            // Membuat session untuk menyimpan data username
+            $_SESSION['username'] = $username;
+
+            // Membuat session untuk menyimpan data password
+            $_SESSION['password'] = $password;
+
+            // Membuat session untuk menyimpan data level
+            $_SESSION['level'] = $level;
+
+            // Redirect ke halaman index.php
+            header("location:index.php");
+
+        // Jika username dan password tidak ada di database
+        }else{
+
+            // Redirect ke halaman login.php
+            header("location:login.php");
+
+        }
+    };
+
+    // Membuat form Login
+    echo "<form action='' method='POST'>
+        <table>
+            <tr>
+                <td>Username</td>
+                <td>:</td>
+                <td><input type='text' name='username'></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td>:</td>
+                <td><input type='password' name='password'></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td><input type='submit' name='login' value='Login'></td>
+            </tr>
+        </table>
+    </form>";
+    
 ?>
